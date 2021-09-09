417b5c9dc52ac195c0ca1863a87a579123d220de
plifis <nikich85@inbox.ru>
2021-09-09 19:14:50 +0300

    0. Spring DataSource [#6878]

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE accident (
+    id serial primary key;
+    name varchar(2000),
+-- text varchar (2000),
+-- address varchar(1000),
+
+);
--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -3,20 +3,33 @@ package ru.job4j.accident.control;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.accident.repository.AccidentJdbcTemplate;
 import ru.job4j.accident.service.AccidentService;
 
 
 @Controller
 public class IndexControl {
-    private AccidentService service;
+//    private AccidentService service;
+//
+//    public IndexControl(AccidentService service) {
+//        this.service = service;
+//    }
+//
+//    @GetMapping("/")
+//    public String index(Model model) {
+//        model.addAttribute("accidents", service.getAllAccident());
+//        return "index";
+//    }
+    private final AccidentJdbcTemplate accidents;
 
-    public IndexControl(AccidentService service) {
-        this.service = service;
+    public IndexControl(AccidentJdbcTemplate accidents) {
+        this.accidents = accidents;
     }
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("accidents", service.getAllAccident());
+        model.addAttribute("accidents", accidents.getAll());
         return "index";
     }
+
 }
